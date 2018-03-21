60299c711b5c75921383bfa7c0dc4fe2edb9f0da
ryah.belford <ryah.belford@simmons.edu>
2018-03-20 20:48:49 -0400

    First JavaScript in-class assignment

--- week-6/in-class/js/learning.js
+++ week-6/in-class/js/learning.js
@@ -1,2 +1,34 @@
 // "use strict" tells the browser to enforce some rules about what can be in our JavaScript.
 "use strict";
+
+let my_name = "Ryah";
+let your_name = "Chris";
+
+if (my_name === "Ryah") {
+  renderOutput("That's me!");
+} else if (my_name === "Chris")
+  renderOutput("That's Chris!");
+} else {
+  renderOutput("I must be someone else");
+}
+
+function helloWorld() {
+  renderOutput("Hello World");
+}
+
+helloWorld();
+
+functinoary helloName(name) {
+  renderOutput("Hello " + name);
+}
+
+helloName("Ryah");
+helloName("Bob");
+
+function todaysHours(days_hours) {
+  renderOutput(days_hours);
+}
+
+todayshours("Tuesday: 9a - 5p");
+
+hours.forEach(todaysHours);
